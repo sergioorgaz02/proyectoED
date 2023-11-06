78cd4447b29969be06c2bf03c3979a766361b567
sorgaz12 <sergioorgaz02@gmail.com>
2023-11-06 14:22:36 +0100

    Buenas Tardes

--- src/uno/HolaMundo.java
+++ src/uno/HolaMundo.java
@@ -3,5 +3,6 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
 		System.out.println("Buenos dias");
+		System.out.println("Buenas tardes");
 	}
 }
